0f6831fa8cdddc96b2adf67d82c09f3ac27a76ed
Davide Zanni <info@dzinformatica.it>
2025-09-28 11:56:01 +0200

    2

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Development\flutter"
-export "FLUTTER_APPLICATION_PATH=E:\Flutter\Package\flutter_barcode_scanner\example"
+export "FLUTTER_ROOT=/Users/davide/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/davide/Desktop/flutter/flutter_barcode_scanner/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.0.0.0"
 export "FLUTTER_BUILD_NUMBER=2.0.0.0"
